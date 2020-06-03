bf07862ebe5caa93b0035e8597db88531e617c01
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-03 19:34:04 +0530

    👊

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -62,7 +62,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 21)
+    max_num_pages = ceil(len(pairs) / 10)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
