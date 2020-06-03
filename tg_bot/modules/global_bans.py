eb8b7c934ce4c0b6f19961e3bdb80b982ceabca7
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-03 18:26:51 +0530

    👊

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -306,7 +306,7 @@ you and your groups by removing spam flooders as quickly as possible. They can b
 /gbanstat
 """
 
-__mod_name__ = "Global Bans"
+__mod_name__ = "GLOBAL BAN"
 
 GBAN_HANDLER = CommandHandler("gban", gban, pass_args=True,
                               filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
