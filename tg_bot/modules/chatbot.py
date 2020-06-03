25f643f6b95e79e5850d5f9eb284c0451eef138c
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-03 18:08:45 +0530

    👊

--- tg_bot/modules/chatbot.py
+++ tg_bot/modules/chatbot.py
@@ -87,7 +87,7 @@ def chatbot(bot: Bot, update: Update):
             bot.send_message(OWNER_ID, f"Chatbot error: {e} occurred in {chat_id}!")
                     
 
-__mod_name__ = "Chatbot"
+__mod_name__ = "CHAT BOT"
 
 __help__ = """
 
