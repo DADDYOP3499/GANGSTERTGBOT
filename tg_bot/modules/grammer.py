bb8e042f35615e5e5a5602c25ef90dbdf578cb1b
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-03 18:31:36 +0530

    👊

--- tg_bot/modules/grammer.py
+++ tg_bot/modules/grammer.py
@@ -52,7 +52,7 @@ __help__ = """
  - /t: while replying to a message, will reply with a grammar corrected version
 """
 
-__mod_name__ = "Grammmar"
+__mod_name__ = "GRAMMER"
 
 
 TRANSLATE_HANDLER = CommandHandler('t', translate)
