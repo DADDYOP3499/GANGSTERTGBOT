6cccccfabea8364d39dc440d67db422337d4d764
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-03 16:55:22 +0530

    Haha

--- tg_bot/modules/emojis.py
+++ tg_bot/modules/emojis.py
@@ -58,7 +58,33 @@ EDIT_SLEEP = 1
 EDIT_TIMES = 11
 
 
-EDIT_TIMES = 11
+#sleep how many times after each edit in 'moonanimation' 
+EDIT_SLEEP = 1
+#edit how many times in 'blockanimation' 
+EDIT_TIMES = 18
+
+
+
+block_chain = [
+             "🟥",
+             "🟧",
+             "🟨",
+             "🟩",
+             "🟦",
+             "🟪",
+             "🟫",
+             "⬛",
+             "⬜",
+             "🟥",
+             "🟧",
+             "🟨",
+             "🟩",
+             "🟦",
+             "🟪",
+             "🟫",
+             "⬛",
+             "⬜"
+]
 
 
 
@@ -182,7 +208,14 @@ earth_ani = [
 ]
 
 
-
+@user_admin
+@run_async
+def blockanimation(bot: Bot, update: Update):
+    msg = update.effective_message.reply_text('⬜') 
+    for x in range(EDIT_TIMES):
+        msg.edit_text(block_chain[x%18])
+        time.sleep(EDIT_SLEEP)
+    msg.edit_text('🟥')
 
 
 
@@ -288,6 +321,8 @@ __help__ = """
 - /clockanimation 🕛
 
 - /earthanimation 🌍
+
+- /blockanimation 🟥
 """
 
 
@@ -297,6 +332,7 @@ HACK_HANDLER = DisableAbleCommandHandler("hack", hack)
 BOMBS_HANDLER =DisableAbleCommandHandler("bombs",bombs)
 MOONANIMATION_HANDLER =DisableAbleCommandHandler("moonanimation",moonanimation)
 CLOCKANIMATION_HANDLER =DisableAbleCommandHandler("clockanimation",clockanimation)
+BLOCKANIMATION_HANDLER =DisableAbleCommandHandler("blockanimation",blockanimation)
 EARTHANIMATION_HANDLER =DisableAbleCommandHandler("earthanimation",earthanimation)
 dispatcher.add_handler(LOVE_HANDLER)
 dispatcher.add_handler(HACK_HANDLER)
@@ -304,8 +340,9 @@ dispatcher.add_handler(BOMBS_HANDLER)
 dispatcher.add_handler(EARTHANIMATION_HANDLER)
 dispatcher.add_handler(MOONANIMATION_HANDLER)
 dispatcher.add_handler(CLOCKANIMATION_HANDLER)
+dispatcher.add_handler(BLOCKANIMATION_HANDLER)
 
 
 __mod_name__ = "EMOJIS"
-__command_list__ = ["love", "hack", "bombs", "moonanimation", "clockanimation", "earthanimation"]
-__handlers__ = [LOVE_HANDLER, HACK_HANDLER, BOMBS_HANDLER, MOONANIMATION_HANDLER, CLOCKANIMATION_HANDLER, EARTHANIMATION_HANDLER]
+__command_list__ = ["love", "hack", "bombs", "moonanimation", "clockanimation", "earthanimation", "blockanimation"]
+__handlers__ = [LOVE_HANDLER, HACK_HANDLER, BOMBS_HANDLER, MOONANIMATION_HANDLER, CLOCKANIMATION_HANDLER, EARTHANIMATION_HANDLER, BLOCKANIMATION_HANDLER]
